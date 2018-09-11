bf78e20c42364091da0d8cf84fec74e8899297f1
mpiot2018 <43101459+mpiot2018@users.noreply.github.com>
2018-09-12 09:59:25 +1000

    Displays 10 text display colors

---
+++ tput.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+clear
+tput setaf 1
+echo " ############################################################# "
+echo " ##   Remove Scenario1 Melbourne Polytechnic IOT Class      ## "
+echo " ############################################################# "
+echo "  tput setaf 1   "
+sleep 2
+tput setaf 2
+echo " ############################################################# "
+echo " ##   Remove Scenario1 Melbourne Polytechnic IOT Class      ## "
+echo " ############################################################# "
+echo "  tput setaf 2   "
+sleep 2
+tput setaf 3
+echo " ############################################################# "
+echo " ##   Remove Scenario1 Melbourne Polytechnic IOT Class      ## "
+echo " ############################################################# "
+echo "  tput setaf 3   "
+sleep 2
+tput setaf 4
+echo " ############################################################# "
+echo " ##   Remove Scenario1 Melbourne Polytechnic IOT Class      ## "
+echo " ############################################################# "
+echo "  tput setaf 4   "
+sleep 2
+tput setaf 5
+echo " ############################################################# "
+echo " ##   Remove Scenario1 Melbourne Polytechnic IOT Class      ## "
+echo " ############################################################# "
+echo "  tput setaf 5   "
+sleep 2
+tput setaf 6
+echo " ############################################################# "
+echo " ##   Remove Scenario1 Melbourne Polytechnic IOT Class      ## "
+echo " ############################################################ "
+echo "  tput setaf 6   "
+sleep 2
+tput setaf 7
+echo " ############################################################# "
+echo " ##   Remove Scenario1 Melbourne Polytechnic IOT Class      ## "
+echo " ############################################################# "
+echo "  tput setaf 7   "
+sleep 2
+tput setaf 8
+echo " ############################################################# "
+echo " ##   Remove Scenario1 Melbourne Polytechnic IOT Class      ## "
+echo " ############################################################# "
+echo "  tput setaf 8  "
+sleep 2
+tput setaf 9
+echo " ############################################################# "
+echo " ##   Remove Scenario1 Melbourne Polytechnic IOT Class      ## "
+echo " ############################################################# "
+echo "  tput setaf 9  "
+sleep 2
+tput setaf 9
+echo " ############################################################# "
+echo " ##   Text is blank                                         ## "
+echo " ############################################################# "
+echo "  tput setaf 0  "
+sleep 2
+
